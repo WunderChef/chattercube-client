f9b94a4f216fc2f984484e95bcc11beb3cf43d65
Philip Marazita <pmarazita@gmail.com>
2017-09-05 15:01:36 -0700

    Update file structure and remove extras from setup
    Create-react-app came with lots of bloat and overhead, started removing some of it and will continue removing as time goes on

---
+++ chattercube-client/src/Components/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <nav className="navbar">
+            <div>
+              Search
+            </div>
+          </nav>
+          <div>
+            <div>
+              <div className="title">Title</div>
+            </div>
+            <div>
+              <div className="message-list">Message list</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
